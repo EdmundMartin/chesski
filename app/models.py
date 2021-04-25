07cce68b6c07ae2629e3e369ae3500086a94b798
EdmundMartin <edmartin101@gmail.com>
2021-04-25 14:36:28 +0100

    Allow for creation of puzzles and loading via API

--- app/models.py
+++ app/models.py
@@ -6,4 +6,5 @@ class ChessPuzzle(db.Model):
     puzzle_name = db.Column(db.String(265))
     starting_position = db.Column(db.String(265))
     orientation = db.Column(db.String(12))
+    # TODO Move to postgres to make use of JSON format
     moves = db.Column(db.Text)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from flask import render_template, send_from_directory
-from app import app
+import json
+from flask import render_template, send_from_directory, request, jsonify
+from app import app, db
+from app.models import ChessPuzzle
 
 
 @app.route('/css/<path:path>')
@@ -25,3 +27,38 @@ def puzzles():
 @app.route('/add-puzzle')
 def add_puzzle():
     return send_from_directory('templates', 'add-puzzle.html')
+
+
+@app.route('/api/save-puzzle', methods=['POST'])
+def save_puzzle():
+    payload = json.loads(request.data)
+    print(payload)
+    name = payload['name']
+    starting_position = payload['startingPosition']
+    moves = json.dumps(payload['moves'])
+    orientation = payload['orientation']
+    """
+    puzzle_name = db.Column(db.String(265))
+    starting_position = db.Column(db.String(265))
+    orientation = db.Column(db.String(12))
+    moves = db.Column(db.Text)
+    
+    puzzle = ChessPuzzle(puzzle_name=name, starting_position=starting_position, moves=moves,
+                         orientation=)
+    """
+    puzzle = ChessPuzzle(puzzle_name=name, starting_position=starting_position, moves=moves,
+                         orientation=orientation)
+    db.session.add(puzzle)
+    db.session.commit()
+    return ''
+
+
+@app.route('/api/load-puzzle', methods=['GET'])
+def load_puzzle():
+    puzzle: ChessPuzzle = ChessPuzzle.query.first()
+    response = {}
+    response['name'] = puzzle.puzzle_name
+    response['startingPosition'] = puzzle.starting_position
+    response['orientation'] = puzzle.orientation
+    response['moves'] = json.loads(puzzle.moves)
+    return jsonify(response)
